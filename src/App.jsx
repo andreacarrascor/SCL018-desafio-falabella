251375c7a2280aee298b2154ab870fde4687adea
andreacarrascor <ancarami19@gmail.com>
2022-03-07 12:35:59 -0300

    Preparación de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="">
+        <h1 className="text-purple-700 font-extrabold text-2xl">Desafío Falabella</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
