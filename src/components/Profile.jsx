59454e19094dcb268c6f95689026752fb13c77a0
andreacarrascor <ancarami19@gmail.com>
2022-03-08 12:55:41 -0300

    avances layout header primario

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Profile = () => {
     return ( 
-        <section className="flex flex-col justify-center items-center border border-black h-563 w-full mt-5 ml-5 mr-20">
+        <section className="profile flex flex-col justify-center items-center border border-black h-563 w-full mt-5 ml-5 mr-20">
             <h1>Comienza a crear el perfil de tu persona favorita</h1>
             <Link to="/basicInformation">Crear nuevo perfil</Link>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,40 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    screens: {
-      
+    colors: {
+      header: '#ff6200',
+      search: '#495867',
+    },
+    borderRadius: {
+      'sr' : '35px',
     },
     extend: {
+      fontFamily: {
+        'lato': 'Lato, sans-serif',
+      },
+      boxShadow: {
+        'bt': '0px 2px 4px rgba(0, 0, 0, 0.05)',
+        'mp': '0px 0px 4px rgba(0, 0, 0, 0.25)',
+        'am': '0px 0px 5px rgba(51, 51, 51, 0.2)',
+      },
       height: {
         '563': '563px',
         '391': '391px',
         '278': '278px',
-        '252': '252px'
+        '252': '252px',
+        '70' : '70px',
+        '46' : '46px',
 
       },
       width: {
         '939': '939px',
-        '881': '881px'
+        '608': '608px',
+        '881': '881px',
+        '321': '321px',
+      },
+      margin: {
+        '419': '419px',
+        '413': '413px'
       }
     },
   },
