f284f830457b4be85ac3e55052018897528ad81f
andreacarrascor <ancarami19@gmail.com>
2022-03-09 17:28:15 -0300

    avances layout datos basicos

--- src/components/BasicInformation.jsx
+++ src/components/BasicInformation.jsx
@@ -7,6 +7,7 @@ import { useContext } from "react";
 import ButtonSendProfile from "./secondary_components/ButtonSendProfile";
 import SexButtons from "./secondary_components/SexButtons";
 import Date from "./secondary_components/Date";
+import fav_people from '../img/icons/fav_people.png'
 
 const BasicInformation = () => {
   const globalContext = useContext(Context);
@@ -24,10 +25,13 @@ const BasicInformation = () => {
   return (
     <main className="profile flex flex-col w-939 mt-5 mr-20">
       <section className="shadow-am w-full  mr-20">
-        <h1 className="text-2xl mt-6 ml-6">Mi persona favorita</h1>
+        <h1 className="text-2xl mt-6 ml-6 pb-2">Mi persona favorita</h1>
         <Link to="/" className="text-fp underline ml-6">Volver a personas favoritas</Link>
         <div className="bg-db h-252 mt-5 ml-7 mb-5 mr-8">
-          <h2>Datos básicos</h2>
+          <div className="flex flex-row items-center p-4">
+            <img src={fav_people} width="32" height="32" alt="favourite people icon" />
+            <h2>Datos básicos</h2>
+          </div>
           <label htmlFor="name" className="whitespace-pre">Su nombre es   </label>
           <input
             type="text"
@@ -37,14 +41,13 @@ const BasicInformation = () => {
             onChange={onChange}
           ></input>
           <p>Es mi</p>
-
           <FamilyButtons
             familyData={familyData}
           />
           <p>Sexo</p>
-            <SexButtons 
-              sexInformation={sexData}/>
-            <label>Fecha de nacimiento:</label>
+          <SexButtons
+            sexInformation={sexData} />
+          <label>Fecha de nacimiento:</label>
           <Date />
         </div>
       </section>
