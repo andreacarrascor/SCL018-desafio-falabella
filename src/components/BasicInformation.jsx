6772b60ec37827a50e18c0bdd566942581d90461
andreacarrascor <ancarami19@gmail.com>
2022-03-09 09:00:48 -0300

    imagenes de layout importadas y avances estilo aside menu

--- src/components/BasicInformation.jsx
+++ src/components/BasicInformation.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import AddEvent from "./AddEvent";
 import data from "../Arrays.json";
-import Botton from "./Botton";
+import FamilyInformationButton from "./FamilyInformationButton";
 
 const BasicInformation = () => {
-    const informationFamily = data.family;
+    const familyInformation = data.family;
     const informationSex = data.sex;
 
 
@@ -12,14 +12,14 @@ const BasicInformation = () => {
         <main className="profile flex flex-col w-939 mt-5 mr-20">
             <section className="shadow-am w-full  mr-20">
                 <h1 className="text-2xl mt-6 ml-6">Mi persona favorita</h1>
-                <Link to="/" className="underline ml-6">Volver a personas favoritas</Link>
+                <Link to="/" className="text-fp underline ml-6">Volver a personas favoritas</Link>
                 <div className="bg-db h-252 mt-5 ml-7 mb-5 mr-8">
                     <h2>Datos básicos</h2>
                     <label>Su nombre es </label>
                     <input type="text" placeholder="Escribe su Nombre"></input>
                     <p>Es mi</p>
 
-                    <Botton informationFamily={informationFamily}
+                    <FamilyInformationButton familyInformation={familyInformation}
                         informationSex={informationSex}
                     />
 
---
+++ src/components/layout/MenuProfile.jsx
@@ -0,0 +1,17 @@
+import profile_icon from '../../img/profile_icon.png'
+import profile_info from '../../img/profile_info.png'
+
+const MenuProfile = () => {
+    return ( 
+        <section className="welcome shadow-mp flex items-center h-24 ml-20 mr-20 mt-23">
+            <div className='ml-16 mr-5'>
+                <img src={profile_icon} width="59" height="59" alt="client icon" />
+            </div>
+            <div>
+                <img src={profile_info} width="231" height="57" alt="client info" />
+            </div>
+        </section>
+    );
+}
+ 
+export default MenuProfile;
